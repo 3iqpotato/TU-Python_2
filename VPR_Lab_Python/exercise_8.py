0325b9fdc522f30f840b025ebe31bfd28e03df38
3iqpotato <103383079+3iqpotato@users.noreply.github.com>
2023-12-04 16:28:59 +0200

    Tasks

--- VPR_Lab_Python/exercise_8.py
+++ VPR_Lab_Python/exercise_8.py
@@ -5,14 +5,14 @@ from VPR_Lab_Python.exercise_8_part_2 import check_number_2
 # with open('test_file', 'r') as f:
 #     print(f.read())
 #     print(f.name)
-
 # try:
 #     a = int(input("Enter a number: "))
 #     b = int(input("Enter a number: "))
 #     c = a / b
-# #     print(f"c=a/b => {a}/{b}={c}")
+#     print(f"c=a/b => {a}/{b}={c}")
 # except ZeroDivisionError:
 #     print("cant divide by zero")
+#
 # except :
 #     print(f'Something went wrong')
 #
@@ -30,18 +30,19 @@ from VPR_Lab_Python.exercise_8_part_2 import check_number_2
 #
 #     print(f'Index {student} is out of range')
 #
+
 # except ValueError:
 #
 #     print(f'Input must be a number')
-
+#
 # finally:
 #     print(f'Done')
 #
 #
 # print(f'Goodbye')
-
-
-
+#
+#
+#
 # num = input('Enter a number: ')
 #
 # try:
@@ -58,41 +59,41 @@ from VPR_Lab_Python.exercise_8_part_2 import check_number_2
 #
 # finally:
 #     print('Good Bye')
-
-def check_number(number):
-
-    if number < 0:
-        raise Exception('The number must be positive')
-
-    else:
-        return math.sqrt(number)
-
-
-try:
-    number = int(input('Enter a number: '))
-    my_num = check_number(number)
-    print(my_num)
-
-except ValueError as ve:
-    print(ve.args[0])
-
-except Exception as ex:
-    print(f"{ex.args[0]}")
-
-# import random
 #
-# grade = random.randint(2, 6)
-# print(grade)
+# def check_number(number):
+#
+#     if number < 0:
+#         raise Exception('The number must be positive')
+#
+#     else:
+#         return math.sqrt(number)
+#
+#
+# try:
+#     number = int(input('Enter a number: '))
+#     my_num = check_number(number)
+#     print(my_num)
+#
+# except ValueError as ve:
+#     print(ve)
+#
+# except Exception as ex:
+#     print(f"{ex}")
+#
+# # import random
+# #
+# # grade = random.randint(2, 6)
+# # print(grade)
 
 #import from other file
 
-try:
-    number = int(input('Enter a number: '))
-    my_num = check_number_2(number)
-    print(my_num)
+# try:
+#     number = int(input('Enter a number: '))
+#     my_num = check_number_2(number)
+#     print(my_num)
+#
+# except (Exception, ValueError) as ex:
+#     print(ex)
 
-except ValueError as ve:
-    print(ve.args[0])
-
-except Exception as ex:
-    print(f"{ex.args[0]}")
+# except Exception as ex:
+#     print(f"{ex}")
